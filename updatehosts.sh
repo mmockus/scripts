ea8f689a601f18ccb5951d42ce7c9c1d2f7d794b
Michael Mockus <mmockus@gmail.com>
2021-07-18 10:06:27 -0500

    add sudo for machine-id

--- updatehosts.sh
+++ updatehosts.sh
@@ -4,6 +4,10 @@
 ## scp .scriptlocation username@serverip:updatehosts.sh
 ## remote host usage
 ## sudo ./updatehosts.sh $HOSTNAME <<newname>>
+## https://git.io/JWQWt
+## curl https://raw.githubusercontent.com/mmockus/scripts/main/updatehosts.sh --output updatehosts.sh
+## curl https://raw.githubusercontent.com/mmockus/scripts/main/updatehosts.sh | sh -s - oldhost newhost
+
 
 oldname=$1
 newname=$2
@@ -19,9 +23,9 @@ sudo hostnamectl set-hostname $newname
 ## update the hosts 
 sudo sed -i "s/$oldname/$newname/g" /etc/hosts
 ## Reset machineId
-rm -f /etc/machine-id /var/lib/dbus/machine-id
-dbus-uuidgen --ensure=/etc/machine-id
-dbus-uuidgen --ensure
+sudo rm -f /etc/machine-id /var/lib/dbus/machine-id
+sudo dbus-uuidgen --ensure=/etc/machine-id
+sudo dbus-uuidgen --ensure
 ## regen SSH
 ## regen ssh keys
 sudo rm /etc/ssh/ssh_host_*
